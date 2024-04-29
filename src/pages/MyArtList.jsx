5269f772d632ca0eff4dd6d9ad223b663c7f5e3e
sioanik <sio.anik.007@gmail.com>
2024-04-29 23:34:36 +0600

    my crafts filter done

--- src/pages/MyArtList.jsx
+++ src/pages/MyArtList.jsx
@@ -8,26 +8,40 @@ const MyArtList = () => {
 
     const { user } = useContext(AuthContext)
 
+    const [myAllItem, setMyAllItem] = useState([])
     const [myItem, setMyItem] = useState([])
 
     // const remaining = myItem.filter(item => item._id !== id )
 
-    const custAvailable = myItem.filter(item => item.customization == 'yes')
+    const custAvailable = myAllItem.filter(item => item.customization == 'yes')
     console.log(custAvailable);
 
-    const custUnavailable = myItem.filter(item => item.customization == 'no')
+    const custUnavailable = myAllItem.filter(item => item.customization == 'no')
     console.log(custUnavailable);
 
-    
+
+   
+
 
     useEffect(() => {
         fetch(`http://localhost:5000/mycrafts/${user?.email}`)
             .then(res => res.json())
             .then(data => {
+                setMyAllItem(data)
                 setMyItem(data)
                 console.log(data);
             })
     }, [])
+
+
+    const handleCustomizable = () =>{
+        setMyItem(custAvailable)
+    }
+
+    const handleNotCustomizable = () =>{
+        setMyItem(custUnavailable)
+    }
+
 
     const handleDeleteCraft = (id) => {
 
@@ -57,14 +71,14 @@ const MyArtList = () => {
                         .then(data => {
                             console.log(data);
                             if (data.deletedCount > 0) {
-                                const remaining = myItem.filter(item => item._id !== id )
+                                const remaining = myItem.filter(item => item._id !== id)
                                 setMyItem(remaining)
                                 Swal.fire({
                                     title: "Deleted!",
                                     text: "Your file has been deleted.",
                                     icon: "success"
                                 });
-                                
+
                             }
 
                         })
@@ -87,37 +101,48 @@ const MyArtList = () => {
                 <p className="text-center">Embark on a journey of personalized creativity as you delve into our exclusive collection of your own crafted treasures. Explore unique crafts and items meticulously crafted with your passion, ready to fuel your next creative endeavor.</p>
             </div>
             <div>
-                {
-                    myItem.map((item, idx) => <div key={idx}>
-                        <div className="card bg-base-100 py-10 shadow-xl">
-                            <figure><img src={item.image} className="max-h-48" alt="Shoes" /></figure>
-                            <div className="text-center card-body">
-                                <h2 className="card-title mx-auto">
-                                    {item.item_name}
-                                    <div className="badge badge-primary">{item.rating} <span className="pl-1"><FaStar /></span></div>
-                                </h2>
-                                <h2 className="mx-auto card-title text-red-500"> Price-
-                                    <span>${item.price}</span>
-                                </h2>
-                                <p>{item.description}</p>
-                                <p>Customization- <span>{item.customization}</span></p>
+                <div className="flex justify-center mb-20">
+                    <div className="dropdown dropdown-hover">
+                        <div tabIndex={0} role="button" className="btn m-1">Customization Filter</div>
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                            <li onClick={handleCustomizable}><a>Customizable</a></li>
+                            <li onClick={handleNotCustomizable}><a>Not Customizable</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    {
+                        myItem.map((item, idx) => <div key={idx}>
+                            <div className="card bg-base-100 py-10 shadow-xl">
+                                <figure><img src={item.image} className="max-h-48" alt="Shoes" /></figure>
+                                <div className="text-center card-body">
+                                    <h2 className="card-title mx-auto">
+                                        {item.item_name}
+                                        <div className="badge badge-primary">{item.rating} <span className="pl-1"><FaStar /></span></div>
+                                    </h2>
+                                    <h2 className="mx-auto card-title text-red-500"> Price-
+                                        <span>${item.price}</span>
+                                    </h2>
+                                    <p>{item.description}</p>
+                                    <p>Customization- <span>{item.customization}</span></p>
 
-                                <p>Stock Status- <span>{item.stockStatus}</span></p>
+                                    <p>Stock Status- <span>{item.stockStatus}</span></p>
 
-                                <div className="card-actions justify-center gap-10 flex pt-4">
-                                    <div className="card-actions">
-                                        <Link to={`/update-craft/${item._id}`}><button className="btn btn-primary">Update</button></Link>
+                                    <div className="card-actions justify-center gap-10 flex pt-4">
+                                        <div className="card-actions">
+                                            <Link to={`/update-craft/${item._id}`}><button className="btn btn-primary">Update</button></Link>
 
-                                    </div>
-                                    <div className="card-actions">
-                                        <button onClick={() => handleDeleteCraft(item._id)} className="btn btn-primary">Delete</button>
+                                        </div>
+                                        <div className="card-actions">
+                                            <button onClick={() => handleDeleteCraft(item._id)} className="btn btn-primary">Delete</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>)
-                }
+                        </div>)
+                    }
 
+                </div>
             </div>
         </div>
     );
